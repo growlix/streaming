4f3beac8c937e9aeba39cb29a61e8ec825431849
Matthew Leavitt <matthew@mosaicml.com>
2023-02-05 02:09:12 +0000

    debug print

--- scripts/deduplication/approximate/get_embeddings.py
+++ scripts/deduplication/approximate/get_embeddings.py
@@ -122,7 +122,11 @@ def avg_pool_tokens(last_hidden_states: Tensor,
 
 def avg_sequences(seq_embeddings: Tensor, sample_indices: Tensor):
     curr_device = seq_embeddings.device
-    sample_indices = sample_indices.to(curr_device)
+    try:
+        sample_indices = sample_indices.to(curr_device)
+    except:
+        print(sample_indices)
+        print(curr_device)
     uniques, inverse = sample_indices.unique(return_inverse=True)
     reduce_inds = inverse.view(inverse.size(0), 1).expand(-1, seq_embeddings.size(1))
     mean_embeddings = torch.zeros(uniques.size(0), seq_embeddings.size(1), device=curr_device)
