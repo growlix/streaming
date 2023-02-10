5bcb53c632cb535f6219c59d7f1a008a87e4c027
Matthew Leavitt <matthew@mosaicml.com>
2023-02-09 17:53:59 -0800

    kmeans and pairwise sim

---
+++ scripts/deduplication/approximate/kmeans.py
@@ -0,0 +1,111 @@
+import faiss
+import torch
+import time
+import numpy as np
+from torch.nn.functional import normalize
+import logging
+from tqdm import tqdm
+import pandas as pd
+import os
+import pickle
+
+get all the emb - use "pretrained" openclip/ or the Kushal used - store them as np memmap - don't forget to (layer) normalize each single emb (use torch.normalize)
+
+kmeans clustering with faiss, choose the number of cluster, 50K, 20K
+
+emb_array = np.memmap('EMB_MEMORY.npy', dtype='float32', mode='w+', shape=SHAPE)
+emb_array[0] = .....
+emb_array = np.memmap('EMB_MEMORY.npy', dtype='float32', mode='r+', shape=SHAPE)
+emb_array[0]
+
+
+def kmeans_clustering(data: np.ndarray, paths_list: np.ndarray , ncentroids: int=1000, niter: int=100, seed: int=1234,
+                      verbose: bool=True, use_clusters_bal: bool=True, sim_metric: str='l2', keep_hard: bool=True,
+                      use_supervised_prototypes: bool=False, Kmeans_with_cos_dist: bool=False, kmeans_obj_file="", logger: logging.Logger=None) -> list:
+    '''
+    Runs Kmeans clustering using "faiss", and ranks each cluster/class items using the 
+    distance to the cluster centorid.
+    args:
+        data (embs): numpy array of shape [dataset_size x d], d is th representation vector size.
+        ncentroids: number of centroids.
+        niter: Kmeans clustering iterations.
+
+    returns:
+        sorted_centroids: list of list, each list represents a cluster/class examples sorted by the distance to
+                        the cluster centroid. Each example is represented by tuple (img id, dist, cluster id)
+    '''
+    # Step 1) Compute Kmeans centroids
+    # -- if kmeans_obj is not created and saved before - > create and train faiss Kmeans clustering object
+    if not os.path.exists(kmeans_obj_file):
+        ## -- data shoud be normalized
+        d = data.shape[1]
+        device = 'cuda:1' if torch.cuda.is_available() else 'cpu'
+        logger.info(f'clustering on {device} ....')
+        
+        spherical = True  # spherical=True when Kmeans_with_cos_dist is True
+        kmeans = faiss.Kmeans(d, ncentroids, niter=niter, verbose=verbose, seed=seed, spherical= spherical, gpu=True) # faiss.Kmeans "gpu" argument: bool or int, optional. False: don't use GPU, True: use all GPUs, number: use this many GPUs.
+        st = time.time()
+        kmeans.train(data)
+        logger.info(f'time for clustering (mins): {(time.time()-st)/(60)}')
+        if kmeans_obj_file != "":
+            with open(kmeans_obj_file, 'wb') as file:
+                pickle.dump(kmeans, file)
+    elif kmeans_obj_file != "":
+        # -- Else, load stored kmeans object
+        logger.info(f'Loading faiss Kmeans pickle file from {kmeans_obj_file}')
+        with open(kmeans_obj_file, 'rb') as file:
+            kmeans = pickle.load(file)
+
+    # Step 2) Find the nearest centroid for each data point, l2 distance search
+    logger.info('nearest centroids...')
+    st = time.time()
+    dist_to_cent, nearest_cent = kmeans.index.search(data, 1) # nearest_cent: the nearest centroid for each example in data. dist_to_cent: contains the squared L2 distances.
+    dist_to_cent, nearest_cent = dist_to_cent.squeeze(1), nearest_cent.squeeze(1)
+    logger.info(f"time to find nearest centroids: {(time.time()-st)/60}")
+
+    # Step 3) sort each class/cluster
+    logger.info('Ranking...')
+    st = time.time()
+    if use_clusters_bal: # for cluster balancing
+        assert use_supervised_prototypes is False, "use_clusters_bal requires use_supervised_prototypes=False"
+        df = pd.DataFrame({'paths_list': paths_list, 'nearest_cent':nearest_cent, 'dist_to_cent':dist_to_cent})
+        sorted_clusters = rank_within_cluster_df(data, df, kmeans.centroids, sim_metric, keep_hard, spherical)
+        # sorted_clusters = rank_within_cluster(data, paths_list, kmeans.centroids, nearest_cent, dist_to_cent, sim_metric, keep_hard, spherical)
+        logger.info(f'time for ranking {(time.time()-st)/60} mins')
+        return sorted_clusters
+
+
+def rank_within_cluster_df(data, df, centroids: np.ndarray, sim_metric: str, keep_hard: bool=True, spherical: bool=False) -> list:
+    """
+    Sorts each cluster items by the distance to the cluster centroid
+    """
+
+    assert sim_metric in ['cosine', 'l2'], "sim_metric should be one of ['cosine', 'l2']"
+
+    sorted_clusters = []
+    for cluster_c in tqdm(range(len(centroids))): 
+        # ids = (nearest_cent==cluster_c)  # boolean array: True for the examples in cluster c
+        cluster_df = df.loc[df['nearest_cent'] == cluster_c]
+
+        cluster_items = list(cluster_df.index) #np.where(ids)[0] # ids of examples in cluster c
+        if sim_metric=='cosine':
+            if spherical:
+                cluster_dists_to_cent = list(1 - cluster_df['dist_to_cent'])
+            else:
+                cluster_c_centroid = torch.Tensor(centroids[cluster_c])
+                sim_to_cent = torch.nn.CosineSimilarity(dim=1)(torch.Tensor(data[cluster_items]), cluster_c_centroid)
+                cluster_dists_to_cent = (1-sim_to_cent).tolist()
+
+        elif sim_metric=='l2': # get the l2 distance from "dist_to_cent" array
+            cluster_dists_to_cent = list(cluster_df['dist_to_cent'])
+
+        cluster_label = np.full((len(cluster_df)), cluster_c).tolist()
+        # in_labels = [img_name.split('_')[0] for img_name in paths_list[ids]]
+        images_names = list(cluster_df['paths_list'])
+        sort_descending = keep_hard
+        cluster_sorted = sorted(zip(images_names, cluster_items, cluster_dists_to_cent, cluster_label), key=lambda x: x[2], reverse=sort_descending) # sort_descending = True for descending sort
+            
+        sorted_clusters.append(cluster_sorted) #  Descending dists. list of of list of tuples (example, dist). The ith list of tuples corresponds to cluster i
+    
+    return sorted_clusters
+
---
+++ scripts/deduplication/approximate/pairwise_sim.py
@@ -0,0 +1,126 @@
+import numpy as np
+from tqdm import tqdm
+import os
+import torch
+import pickle
+import pandas as pd
+import random
+
+def _encode_shard(self, shard, sorted_clusters_path):
+    def init_memmap_laion440_images_embs():
+        STR_DTYPE = "S24"
+        EMB_MEMORY = "/checkpoints/amroabbas/datapruning/open-clip-encodings/laion440m/laion440m_ViT-B-16_embs.npy"
+        dataset_size = 437444349
+        SHAPE = (dataset_size, 512)
+        embs = np.memmap(EMB_MEMORY, dtype='float32', mode='r', shape=SHAPE)
+
+        return embs
+    
+    embs = init_memmap_laion440_images_embs()
+    
+
+    
+
+    end = min(50000, shard+self.args.clusters_per_job)
+
+    def contains_duplicates(arr):
+        return len(np.unique(arr)) != len(arr)
+
+    for cluster_id in tqdm(range(shard, end)):
+        big_clusters = [24224, 31847] 
+        if cluster_id in big_clusters:
+            continue     
+        dict_file_loc = os.path.join(self.args.save_loc, f"dicts/cluster_{cluster_id}.pt")
+        df_file_loc = os.path.join(self.args.save_loc, f"dataframes/cluster_{cluster_id}.pkl")
+
+        if os.path.exists(dict_file_loc) and os.path.exists(df_file_loc):
+            print(f"{dict_file_loc} exists, moving on")
+            continue
+        ## -- load cluster i representations
+        cluster_i = np.load(os.path.join(sorted_clusters_path, f'cluster_{cluster_id}.npy'))
+        # 1) store cluster size
+        cluster_size = cluster_i.shape[0]
+
+        ## -- indices for cluster items in the dataset
+        cluster_ids = cluster_i[:, 1].astype('int32')
+
+        cluster_reps = embs[cluster_ids]
+        cluster_reps = torch.tensor(cluster_reps)
+
+        ## -- compute pairwise cos sim between cluster items, then replace to diagonal with zeros to ignore self similarity
+        pair_w_sim_matrix = (cluster_reps @ (cluster_reps.T))
+        pair_w_sim_matrix.fill_diagonal_(0.0)
+        assert pair_w_sim_matrix.shape[0]==pair_w_sim_matrix.shape[1]
+
+        if True:
+            ## -- get paths to cluster i images
+            image_urls = cluster_i[:, 0]
+
+            ## -- make sure all the paths are unique this ensure that the duplicates are really stored many time times on memory
+            assert not contains_duplicates(image_urls)
+            
+            ## -- 2) compute the sum of all pairwise sim values exept the diagonal (diagonal items = 1.0)
+            # sim_sum = torch.sum(pair_w_sim_matrix)-(cluster_size*1.0) # torch.sum(pair_w_sim_matrix.flatten()[1:].view(n-1, n+1)[:,:-1])
+            # avg_pair_w_sim = sim_sum/((cluster_size**2)-cluster_size)
+            # sim_sum = torch.sum(triu)
+            # avg_pair_w_sim = sim_sum/(cluster_size*(cluster_size-1)/2)
+            avg_sim_to_others_list = (1/(cluster_size-1))*(torch.sum(pair_w_sim_matrix, dim=0)) # -- array of shape (cluster_size x 1)
+
+            ##-- 3) compute max pairwise similarity
+            max_pair_w_sim_list = torch.max(pair_w_sim_matrix, dim=0)[0] # -- array of shape (cluster_size x 1)
+            min_pair_w_sim_list = torch.min(pair_w_sim_matrix, dim=0)[0] # -- array of shape (cluster_size x 1)
+            std_pair_w_sim = pair_w_sim_matrix.std()
+
+            ## -- 4) average value of cos similarity to cluster centroid
+            avg_sim_to_cent = (1-cluster_i[:, 2].astype('float32')).mean()
+            std_sim_to_cent = (1-cluster_i[:, 2].astype('float32')).std()
+
+        ## -- We need upper tringular matrix because (1)we don't need to look at self sim (always=1) (2)we need the compinations not permutations
+        triu = torch.triu(pair_w_sim_matrix, diagonal=1)
+                    
+        ## -- if the max sim between one example and any other example is > 1-eps, remove this example
+        M = torch.max(triu, dim=0)[0]
+
+        points_to_remove_df = pd.DataFrame()
+
+        values_for_eps = {}
+
+        for eps in self.args.eps_list:
+            ## -- 5) We need to remove a point from the dataset when its pairwise similarity to other point is > 1-ebs
+            eps_points_to_remove = M>1-eps
+            points_to_remove_df[f'eps={eps}'] = eps_points_to_remove
+            
+            if True:
+                ## -- 6) num duplicates in this cluster
+                eps_num_duplicates = sum(eps_points_to_remove).item()
+
+                ## -- 7) duplicates ratio %
+                eps_duplicates_ratio = 100*eps_num_duplicates/cluster_size 
+                ## -- 8) number of similar points to each point
+                eps_num_duplicates_for_each_point = 1 + torch.sum(pair_w_sim_matrix>1-eps, dim=0) # -- array of shape (cluster_size x 1)
+                ## -- store all the value computed for this eps
+                values_for_eps = {'duplicates_ratio': eps_duplicates_ratio, 'num_duplicates': eps_num_duplicates,
+                                    'eps_num_duplicates_for_each_point': eps_num_duplicates_for_each_point}
+                                                
+        num_duplicates_in_cluster_i = {
+                                        'values_for_eps': values_for_eps,
+                                        'cluster_size': cluster_size,
+                                        'cluster_id': cluster_id,
+                                        'avg_sim_to_cent': avg_sim_to_cent,
+                                        'std_sim_to_cent': std_sim_to_cent,
+                                        'std_pair_w_sim': std_pair_w_sim,
+                                        'avg_sim_to_others_list': avg_sim_to_others_list,
+                                        'max_pair_w_sim_list': max_pair_w_sim_list,
+                                        'min_pair_w_sim_list': min_pair_w_sim_list
+                                        }
+
+        if self.args.save_loc != "":
+            ## -- save dict
+            torch.save(num_duplicates_in_cluster_i, dict_file_loc)
+            ## --save df
+            with open(df_file_loc, 'wb') as file:
+                pickle.dump(points_to_remove_df, file)
+
+
+        print('DONE cluster_id ', cluster_id)
+
