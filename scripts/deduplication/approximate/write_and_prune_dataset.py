cdc23d3b04d77f976321f896012177f82165ee18
matthew <matthew@c128m20481-z1-01.prod.z1.r1.mosaicml.cloud>
2023-02-28 01:50:32 +0000

    log data stats

--- scripts/deduplication/approximate/write_and_prune_dataset.py
+++ scripts/deduplication/approximate/write_and_prune_dataset.py
@@ -8,6 +8,10 @@ from tqdm import tqdm
 from streaming.base import MDSWriter, StreamingDataset
 
 
+def running_average(avg: float, new_val: float, count: int):
+    return avg*(count-1)/count + new_val/count
+
+
 def write_and_prune(
     streaming_remote: str,
     streaming_local: str,
@@ -16,6 +20,7 @@ def write_and_prune(
     name: str,
     similarity_file: str,
     keep_fraction: float,
+    source_key: str,
 ):
 
     with open(similarity_file, 'rb') as handle:
@@ -45,10 +50,35 @@ def write_and_prune(
 
     # TODO: Use UIDs instead of index
     # Write deduplicated samples
+    removed = 0
+    data_stats = {
+        'source': {},
+        'n_samples': 0,
+        'mean_length': 0,
+    }
     with MDSWriter(dirname=save_dir, columns=columns, compression=compression, hashes=hashes, size_limit=size_limit) as out:
         for i, sample in tqdm(enumerate(old_dataset), total=len(old_dataset)):
             if similarities['similarities'].get(i, 0) < keep_threshold:
                 out.write(sample)
+                data_stats['n_samples'] += 1
+                sample_len = len(sample['text'])
+                data_stats['mean_length'] = running_average(data_stats['mean_length'], sample_len, i+1)
+                if source_key in sample.keys():
+                    source = sample[source_key]
+                    source_stats = data_stats['source'].get(source, {'n_samples': 0, 'mean_length': 0})
+                    source_stats['n_samples'] += 1
+                    source_stats['mean_length'] = running_average(source_stats['mean_length'], sample_len, i+1)
+                    data_stats['source'][source] = source_stats
+            else:
+                removed += 1
+            if i%2000000 == 0:
+                print(f'\nRemoved {removed} of {i} samples)')
+    print(f'\nRemoved {removed} of {len(old_dataset)} samples)')
+
+    savename = os.path.join(save_dir, 'data_stats.pkl')
+    with open(savename, 'wb') as handle:
+        pickle.dump(data_stats, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    print(f'\nSaved data stats to {savename}')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Write and prune dataset')
@@ -59,6 +89,7 @@ if __name__ == '__main__':
     parser.add_argument('--split', type=str)
     parser.add_argument('--similarity_file', type=str)
     parser.add_argument('--keep_fraction', type=float)
+    parser.add_argument('--source_key', type=str, default='pile_set_name', help='Key indicating data source of sample')
     args = parser.parse_args()
 
     args.save_dir = os.path.join(args.save_dir, args.split)
